fd0cf439073ecf36c325aa8ed2144f4276fd184e
rajabiseraji <rajabiseraji@aut.ac.ir>
2019-02-11 03:00:52 +0330

    chore(vue-config): remove comments

--- vue.config.js
+++ vue.config.js
@@ -12,19 +12,4 @@ module.exports = {
             },
         },
     },
-    // chainWebpack: (config) => {
-    //     module: {
-    //       rules: [{
-    //         test: /\.scss$/,
-    //         use: ['style-loader', 'css-loader', 'sass-loader', 'postcss-loader', {
-    //           loader: 'style-resources-loader',
-    //           options: {
-    //             patterns: [
-    //               path.resolve(__dirname, '~/src/styles/scss/imports.scss'),
-    //             ]
-    //           }
-    //         }]
-    //       }]
-    //     },
-    // },
 };
