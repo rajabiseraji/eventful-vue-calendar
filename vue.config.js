8b14a55abbdc201ba2756b779b1ebd968f846973
rajabiseraji <rajabiseraji@aut.ac.ir>
2019-02-11 02:24:06 +0330

    build(styling): adds global scss support

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,30 @@
 /**
  * Vue config
  */
+
 module.exports = {
-    // options
+    css: {
+        loaderOptions: {
+            sass: {
+                data: `
+                    @import "@/styles/scss/main.scss";
+                    `,
+            },
+        },
+    },
+    // chainWebpack: (config) => {
+    //     module: {
+    //       rules: [{
+    //         test: /\.scss$/,
+    //         use: ['style-loader', 'css-loader', 'sass-loader', 'postcss-loader', {
+    //           loader: 'style-resources-loader',
+    //           options: {
+    //             patterns: [
+    //               path.resolve(__dirname, '~/src/styles/scss/imports.scss'),
+    //             ]
+    //           }
+    //         }]
+    //       }]
+    //     },
+    // },
 };
