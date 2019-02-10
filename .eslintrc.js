e2d48354bd9e49baf413db37afeab951f65275c1
rajabiseraji <rajabiseraji@aut.ac.ir>
2019-02-11 02:55:33 +0330

    build(eslint): standardize eslint options

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,18 +1,33 @@
 module.exports = {
     root: true,
-    env: {
-        node: true,
-    },
     extends: [
-        'plugin:vue/essential',
+        'plugin:vue/recommended',
         '@vue/airbnb',
     ],
-    rules: {
-        'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-        'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-        indent: ['error', 4],
+    plugins: [
+        'vue',
+        'jest',
+    ],
+    env: {
+        browser: true,
+        node: true,
+        'jest/globals': true,
     },
     parserOptions: {
         parser: 'babel-eslint',
+        ecmaVersion: 8,
+        sourceType: 'module',
+    },
+    rules: {
+        'import/prefer-default-export': 0,
+        radix: 0,
+        indent: ['error', 4],
+        'vue/html-indent': ['error', 4],
+        'max-len': [2, {
+            code: 300,
+            tabWidth: 4,
+            ignoreUrls: true,
+            ignoreComments: true,
+        }],
     },
 };
