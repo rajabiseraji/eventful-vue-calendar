d4f79f7111d84a7481e4d18a376965c600803e86
rajabiseraji <rajabiseraji@aut.ac.ir>
2019-02-11 03:15:34 +0330

    chore(date-helper): creates date helper file

---
+++ src/helpers/date.js
@@ -0,0 +1,11 @@
+import dayjs from 'dayjs';
+import isBetween from 'dayjs/plugin/isBetween';
+import jalaliday from 'jalaliday';
+
+dayjs.extend(isBetween);
+dayjs.extend(jalaliday);
+
+export {
+    dayjs as
+    default,
+};
