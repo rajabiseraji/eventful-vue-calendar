219f4e42558f4ffd47a7381192c6a31b0060a54f
rajabiseraji <rajabiseraji@aut.ac.ir>
2019-02-12 16:32:27 +0330

    feat(GridView): inits the base props of the gridView component

--- src/components/core/GridView.vue
+++ src/components/core/GridView.vue
@@ -4,7 +4,82 @@
 
 <script>
 export default {
-
+    props: {
+        numberOfColumns: {
+            type: Number, 
+            default: 7
+        },
+        hasColumnLabels: {
+            type: Boolean,
+            default: true
+        },
+        hasRowLabels: {
+            type: Boolean, 
+            default: false
+        },
+        columnNames: {
+            type: [Array, Object],
+            /**
+             * This must be changed to return an array of all default weekdays from our date library
+             * for now we can handle it in the asyncData and use a helper function or a data factory prop 
+             * to fill it
+             */
+            default: () => [] 
+        },
+        /**
+         * A factory function that makes the days of the week 
+         * this function has precedence over the columnNames prop
+         */
+        columnNameFactory: {
+            type: Function,
+            default: () => {}
+        },
+        rowNames: {
+            type: [Array, Object],
+            default: () => [] 
+        },
+        /**
+         * A factory function that makes the days of the week 
+         * this function has precedence over the columnNames prop
+         */
+        rowNameFactory: {
+            type: Function,
+            default: () => {}
+        },
+        columnLabelClass: {
+            type: String, 
+            default: ''
+        },
+        columnWrapperClass: {
+            type: String, 
+            default: ''
+        },
+        weekendClass: {
+            type: String,
+            default: ''
+        },
+        /**
+         * 
+         * 
+         * Grid layout props
+         * 
+         * 
+         */
+        numberOfRows: {
+            type: [Number, Function],
+            default: 7
+        },
+        gridViewWrapperClass: {
+            type: String,
+            default: ''
+        },
+        gridType: {
+            validator: function (value) {
+                // The value must match one of these strings
+                return ['year', 'month', 'week', 'day'].indexOf(value) !== -1
+            }
+        }
+    }
 }
 </script>
 
